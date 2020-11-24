0e575e12df99d80d0b94c983f7f84e6fe4a18bdd
Jillyn Peterson <jillyn_oc_ca@yahoo.com>
2020-11-24 15:28:33 -0700

    3:30 day one push

--- gamingyourgoals/server/index.js
+++ gamingyourgoals/server/index.js
@@ -1,9 +1,9 @@
 const express = require("express"),
     app = express(),
     ctrl = require("./controller.js"),
-    port = 4040;
+    port = 4036;
 
 app.use(express.json());
 
 
-app.listen(port, () => console.log(`It's working! Who knew??? I'm listening on port ${port}`));
+app.listen(port, () => console.log(`Hello Seattle, I'm listening... on port ${port}`));
