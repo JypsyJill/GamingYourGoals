ea51fdc70d15be3a0ff7eb0404e609915497a40d
Jillyn Peterson <jillyn_oc_ca@yahoo.com>
2020-11-24 16:40:26 -0700

    make it better

---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import React, {Component} from 'react';
+
+class Nav extends Component {
+
+    render() {
+
+        return(
+
+            <div>
+                <h1>Nav</h1>
+            </div>
+        );
+    }
+}
+
+export default Nav;
