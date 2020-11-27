d3437b2cd0b8ff806ecb6adfa49d8777590eb640
Jillyn Peterson <jillyn_oc_ca@yahoo.com>
2020-11-27 14:14:15 -0700

    Friday afternoon push

--- gamingyourgoals/server/index.js
+++ gamingyourgoals/server/index.js
@@ -1,9 +1,41 @@
-const express = require("express"),
-    app = express(),
-    ctrl = require("./controller.js"),
-    port = 4036;
+require('dotenv').config();
+
+const express = require('express');
+const ctrl = require('./controllers/authController')
+const massive = require('massive');
+const session = require('express-session');
+
+const {SESSION_SECRET, SERVER_PORT, CONNECTION_STRING} = process.env;
+const auth = require('./controllers/authController');
+
+const app = express();
 
 app.use(express.json());
 
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: SESSION_SECRET,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+    }
+}))
 
-app.listen(port, () => console.log(`Hello Seattle, I'm listening... on port ${port}`));
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}).then( db => {
+    app.set('db', db)
+    console.log('Hey! Get out of my swamp!')
+}).catch( err => console.log(err));
+
+// Auth endpoints
+app.post('/auth/register', auth.register);
+app.post('/auth/login', auth.login);
+app.post('/auth/logout', auth.logout);
+app.get('/api/user', auth.getUser);
+
+
+app.listen(SERVER_PORT, () => console.log(`Hello Seattle, I'm listening... on port ${SERVER_PORT}`));
