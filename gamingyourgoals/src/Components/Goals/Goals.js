1fa77d7ef2c71a71017b1defcc398e79b899a9f7
Jillyn Peterson <jillyn_oc_ca@yahoo.com>
2020-12-07 09:32:12 -0700

    goals page updated

--- gamingyourgoals/src/Components/Goals/Goals.js
+++ gamingyourgoals/src/Components/Goals/Goals.js
@@ -1,23 +1,23 @@
-// import React, {Component} from 'react';
-// import { Link } from "react-router-dom";
+import React, {Component} from 'react';
+import { Link } from "react-router-dom";
 
 
-// class Goals extends Component {
-//     render() {
-//         return (
-//             <div className="Goals">
-//               <h1>Ready to Game Your Goals?</h1>
-//               <h3>What are we counting? </h3>
-//               <p> When are you starting? </p>
-//               <p> What is your finish date? </p>
-//               <p> How many (words, miles, steps) is your goal? </p>
-//               <p> Do you blackout any days from your progress?</p>
-//               <p> e.g. Sundays, holidays, your anniversary, or spouse's birthday</p>
-//               <p> What is your time zone?</p>
-//               <p> What time would you like to be texted your daily target?</p>
-//             </div>
-//           );
-//         };
-//     }
+class Goals extends Component {
+    render() {
+        return (
+            <div className="Goals">
+              <h1>Ready to Game Your Goals?</h1>
+              <h3>What are we counting? </h3>
+              <p> When are you starting? </p>
+              <p> What is your finish date? </p>
+              <p> How many (words, miles, steps) is your goal? </p>
+              <p> Do you blackout any days from your progress?</p>
+              <p> e.g. Sundays, holidays, your anniversary, or spouse's birthday</p>
+              <p> What is your time zone?</p>
+              <p> What time would you like to be texted your daily target?</p>
+            </div>
+          );
+        };
+    }
 
-// export default Goals;  
+export default Goals;  
