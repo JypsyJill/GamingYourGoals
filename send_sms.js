e2679f6a5706543a912fb856220e84aa33fce7e0
JypsyJill <you@example.com>
2020-12-30 20:09:04 -0700

    okay, so I may have just hosed myself...

---
+++ send_sms.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE_NUMBER, MY_PHONE_NUMBER } = process.env;
+
+const accountSid = TWILIO_ACCOUNT_SID;
+const authToken = TWILIO_AUTH_TOKEN
+
+
+const client = require('twilio')(accountSid, authToken);
+client.messages
+  .create({
+      from: TWILIO_PHONE_NUMBER,
+      to: MY_PHONE_NUMBER,
+      body: 'Your challenge for the day is 2196 words',
+   })
+  .then((message) => console.log(message.sid));
